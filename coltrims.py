6f832b8df4678fec2b618dc0d6481ec7b86ce739
Dennis McGinnis <mcginnis@atom.uni-frankfurt.de>
2024-07-18 11:09:15 +0200

    import reaction in coltrims.py

--- coltrims.py
+++ coltrims.py
@@ -3,6 +3,7 @@ from Constants import CONSTANTS
 from Spectrometer import Spectrometer
 from CalcSettings import CalcSettings
 from Particle import Particle, Ion, Electron, ParticleList
+from Reaction import Reaction
 
 
 
